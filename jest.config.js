5934eb62f9a51b20c0a9c51418500df361b94197
Jim Balhoff <jim@balhoff.org>
2024-10-19 20:23:10 -0400

    Publishing again.

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,10 @@ module.exports = {
   testEnvironment: 'node',
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 91,
+      functions: 91,
+      lines: 91,
+      statements: 91,
     },
   },
 };
